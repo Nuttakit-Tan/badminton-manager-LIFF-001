6a56458a0284c3534b1b18ee5ea71f953e60472d
Nuttakit-Tan <nuttakitkraiphan@gmail.com>
2026-03-13 15:53:05 +0700

    ทำกับเชื่อมข้อมูล

--- public/js/config.js
+++ public/js/config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
   LIFF_ID: "2009440042-jPYs1rPn",
-  API_URL: "ใส่_URL_Apps_Script"
+  API_URL: "https://script.google.com/macros/s/AKfycbwbzoMXjOCJxF9PDq3fGBOG2GRcA2gMCrRpF7-K9SWosURwB1DSqW7c2dwUN0ExpcS8bQ/exec"
 };
--- public/js/user.js
+++ public/js/user.js
@@ -1,8 +1,17 @@
 document.addEventListener("DOMContentLoaded", async () => {
   const profile = await initLiff();
-
   if (!profile) return;
 
   document.getElementById("username").innerText =
     "สวัสดี " + profile.displayName;
+
+  const result = await apiCall("checkUser", {
+    userId: profile.userId
+  });
+
+  if (!result.exists) {
+    alert("ยังไม่เคยสมัคร");
+  } else {
+    alert("เคยสมัครแล้ว");
+  }
 });
